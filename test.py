760525c3a1b711e43f33a4999bcc6d112d9c8ca2
xplago <xplago.main@gmail.com>
2022-12-14 09:47:14 +0300

    Update tests

--- test.py
+++ test.py
@@ -9,6 +9,11 @@ class TestSum(unittest.TestCase):
         result = sum(data)
         self.assertEqual(result, 10, "Should be 10")
 
+    def test_new_list_init(self):
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6, "Should be 6")
+
 
 if __name__ == '__main__':
     unittest.main()
